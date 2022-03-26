cd6d3c74a7f6d46ac76e3a6b23d42d21c3c6d52f
Chiamaka Ojiyi <chiamakaojiyi@gmail.com>
2022-03-26 11:22:54 +0100

    update

--- temp/temp.js
+++ temp/temp.js
@@ -1,14 +1,3 @@
 /***
- *ldsvlmdfmlf;ldfsml;dmf
- *bdsbmnflkdsmfdlnlkkfd
- * hbdjsnlfkslnfldsfglj;fls
- * jdknkdfdjfnvvfd
- * jknsfknldkd;ldsg;dsmf;lpaof
- * jdfnkfsjofjesfsjpoksl;mfspofeporjojoeak'pes'j
- * ldsvlmdfmlf;ldfsml;dmf
- *bdsbmnflkdsmfdlnlkkfd
- * hbdjsnlfkslnfldsfglj;fls
- * jdknkdfdjfnvvfd
- * jknsfknldkd;ldsg;dsmf;lpaof
- * jdfnkfsjofjesfsjpoksl;mfspofeporjojoeak'pes'j
+ *l
  */
